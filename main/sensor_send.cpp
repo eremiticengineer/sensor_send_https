e2d089d41ae2436227ef9999df5b08e9327af48e
eremiticengineer <dev@eremiticengineer.com>
2026-04-17 08:21:05 +0100

    Remove a7670. Now end to end mbedtls

--- main/sensor_send.cpp
+++ main/sensor_send.cpp
@@ -7,7 +7,6 @@
 #include "HttpsClient.h"
 #include "JsonParser.h"
 #include "NvsStorage.h"
-#include "A7670.h"
 
 #include "sdkconfig.h"
 
@@ -24,7 +23,7 @@ extern "C" void app_main()
         vTaskDelay(pdMS_TO_TICKS(1000)); // 1000 ms = 1 second
     }
 
-/*
+    // Test the JSON parsing
     static const char* otaJson = R"(
     {
         "name": "sensor_send_https",
@@ -33,7 +32,6 @@ extern "C" void app_main()
     }
     )";
     JsonParser parser;
-    ESP_LOGI("STACK", "************ Before JSON: %u", uxTaskGetStackHighWaterMark(NULL));
     if (!parser.parse(otaJson)) {
         ESP_LOGE("OTA", "cannot parse JSON");
     }
@@ -43,11 +41,8 @@ extern "C" void app_main()
             parser.getVersion().c_str(),
             parser.getDate().c_str());
     }
-    ESP_LOGI("STACK", "************ After JSON: %u", uxTaskGetStackHighWaterMark(NULL));
-*/
 
-/*
-    ESP_LOGI("STACK", "************ Before NvsStorage: %u", uxTaskGetStackHighWaterMark(NULL));
+    // Test the NVS storage
     NvsStorage storage("ota_data");
     storage.init();
     std::string test = storage.getString("test");
@@ -64,30 +59,25 @@ extern "C" void app_main()
     else {
         ESP_LOGI("OTA", "string exists in nvs \"%s\"", test.c_str());
     }
-    ESP_LOGI("STACK", "************ After NvsStorage: %u", uxTaskGetStackHighWaterMark(NULL));
-*/
 
-/*
-    ESP_LOGI("STACK", "************ Before Wifi Connect: %u", uxTaskGetStackHighWaterMark(NULL));
+    // Connect to wifi
     WifiClient wifi;
     wifi.connect(CONFIG_WIFI_SSID, CONFIG_WIFI_PASSWORD);
-    ESP_LOGI("STACK", "************ After Wifi Connect: %u", uxTaskGetStackHighWaterMark(NULL));
 
-    ESP_LOGI("STACK", "************ Before HTTPS Connect: %u", uxTaskGetStackHighWaterMark(NULL));
+    // Prepare an https client
     HttpsClient client(CONFIG_SENSOR_SEND_WEB_SERVER, CONFIG_SENSOR_SEND_WEB_SERVER_HTTPS_PORT);
-*/
 
-/*
     std::string content;
     std::string headers;
 
-    ESP_LOGI("STACK", "************ Before HTTPS GET: %u", uxTaskGetStackHighWaterMark(NULL));
+    // Connect to the server over https
     if (!client.connect()) {
         ESP_LOGE("SENSOR_SEND", "Failed to connect to server");
         return;
     }
-    ESP_LOGI("STACK", "************ After HTTPS Connect: %u", uxTaskGetStackHighWaterMark(NULL));
-    esp_err_t err = client.get("/iot/sensor_send_https_ota.json", &content, &headers);
+
+    // GET a JSON file from the server over https...
+    esp_err_t err = client.get(CONFIG_SENSOR_SEND_OTA_PATH, &content, &headers);
     client.disconnect();
     if (err != ESP_OK) {
         ESP_LOGE("OTA", "GET failed: %s (%d)", esp_err_to_name(err), err);
@@ -95,9 +85,8 @@ extern "C" void app_main()
         ESP_LOGI("OTA", "Response: %s", content.c_str());
         ESP_LOGI("OTA", "Headers: %s", headers.c_str());
     }
-    ESP_LOGI("STACK", "************ After HTTPS GET: %u", uxTaskGetStackHighWaterMark(NULL));
 
-    ESP_LOGI("STACK", "************ Before JSON response parse: %u", uxTaskGetStackHighWaterMark(NULL));
+    // ...and parse the returned JSON
     const char* json_cstr = content.c_str();
     ESP_LOGI("JSON_FROM_SERVER","%s", content.c_str());
     if (!parser.parse(json_cstr)) {
@@ -109,8 +98,8 @@ extern "C" void app_main()
             parser.getVersion().c_str(),
             parser.getDate().c_str());
     }
-    ESP_LOGI("STACK", "************ After JSON response parse: %u", uxTaskGetStackHighWaterMark(NULL));
 
+    // POST a file to the server over https
     ESP_LOGI("POST", "POSTING...");
     content.clear();
     headers.clear();
@@ -118,35 +107,11 @@ extern "C" void app_main()
         ESP_LOGE("SENSOR_SEND", "Failed to connect to server");
         return;
     }
-    bool response = client.post(CONFIG_SENSOR_SEND_WEB_SERVER_HTTPS_POST_URL,
+    bool response = client.post(CONFIG_SENSOR_SEND_WEB_SERVER_HTTPS_POST_PATH,
+        CONFIG_SENSOR_SEND_WEB_SERVER_HTTPS_POST_API_KEY,
         POST_DATA, &content, &headers);
     client.disconnect();
     ESP_LOGI("POST", "POSTED with response %s", response ? "true" : "false");
     ESP_LOGI("POST", "content: %s", content.c_str());
     ESP_LOGI("POST", "headers: %s", headers.c_str());
-*/
-
-    A7670Modem modem;
-    modem.begin(CONFIG_PHONE_NUMBER_FOR_RESPONSE, "LilyGo A7670 Device Started from esp-idf v6");
-
-    std::string url = CONFIG_SENSOR_SEND_WEB_SERVER_HTTPS_IMAGE_UPLOAD_URL;
-    std::string json = "{\"temperature\":25.3,\"humidity\":60}";
-    std::string apiKey = CONFIG_SENSOR_SEND_WEB_SERVER_HTTPS_IMAGE_UPLOAD_API_KEY;
-    if (modem.httpsPOST(url, json, apiKey)) {
-        ESP_LOGI("MAIN", "POST succeeded");
-    } else {
-        ESP_LOGE("MAIN", "POST failed");
-    }
-
-    url = CONFIG_SENSOR_SEND_OTA_URL;
-    if (modem.httpsGET(url)) {
-        ESP_LOGI("MAIN", "GET succeeded");
-    } else {
-        ESP_LOGE("MAIN", "GET failed");
-    }
-
-    modem.startSMSListener();
-    while(true) {
-      vTaskDelay(pdMS_TO_TICKS(1000));
-    }
 }
