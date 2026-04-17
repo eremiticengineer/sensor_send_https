8f98dcdefc386ffda750aae305a626bd96f899c8
eremiticengineer <dev@eremiticengineer.com>
2026-04-17 08:20:11 +0100

    Add apiKey to post method

--- components/https_client/HttpsClient.h
+++ components/https_client/HttpsClient.h
@@ -10,7 +10,7 @@ public:
     bool connect();
     void disconnect();
     esp_err_t get(const char* path, std::string* out_body = nullptr, std::string* headers = nullptr);
-    bool post(const char* path, const char* json_data, std::string* out_body = nullptr, std::string* out_headers = nullptr);
+    bool post(const char* path, const char* apiKey, const char* json_data, std::string* out_body = nullptr, std::string* out_headers = nullptr);
 
 private:
     //bool write_request(const char* request);
